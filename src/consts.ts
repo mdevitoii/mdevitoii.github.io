81848ec92c44e6612c50e04de257ef8a278effb2
Michael DeVito <mdevitoii04@gmail.com>
2026-09-10 22:45:41 -0400

    poking around

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '/var/log/michael';
+export const SITE_DESCRIPTION = 'Michael DeVito - Personal Portfolio';
